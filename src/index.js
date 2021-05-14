f3099c3e9472052431a4028374c17a5df8978af3
reyestimesbts <juanr@bluetrailsoft.com>
2021-05-14 07:48:27 -0500

    update carrousel behavior

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./example.scss";
 
 (function () {
   document.addEventListener("DOMContentLoaded", function (event) {
-    //do work
     const videosListWrp = document.querySelector(".videosListWrp");
     const leftButton = document.querySelector(".leftButton");
     const rightButton = document.querySelector(".rightButton");
@@ -13,23 +12,57 @@ import "./example.scss";
     let counter = 1;
 
     initVideo();
+    initEvnts();
 
-    leftButton.addEventListener("click", function () {
-      moveContainer("left");
-    });
+    function isAlowedToMove(side, counter) {
+      return (
+        (side === "left" && counter > 1) || (side === "right" && counter < 3)
+      );
+    }
 
-    rightButton.addEventListener("click", function () {
-      moveContainer("right");
-    });
+    function getVideoWidth() {
+      return videosListWrp.querySelector(".video").offsetWidth + 5;
+    }
+
+    function translateContainer(width, side) {
+      const translate = counter * width - (side === "left" ? width : 0);
+      videosListWrp.style.transform = `translateX(-${translate}px)`;
+    }
+
+    function initEvnts() {
+      leftButton.addEventListener("click", function () {
+        moveContainer("left");
+      });
+
+      rightButton.addEventListener("click", function () {
+        moveContainer("right");
+      });
+
+      window.addEventListener("resize", function () {
+        videosListWrp.style.transform = `translateX(0px)`;
+        filler.style.width = "33%";
+        counter = 1;
+      });
+
+      document
+        .querySelector(".mainCarousel")
+        .addEventListener("scroll", function (e) {
+          const container = e.target;
+          const width = container.scrollWidth - container.clientWidth;
+          const percentage = container.scrollLeft / width;
+
+          console.log(percentage);
+        });
+    }
 
     function moveContainer(side) {
       if (isAlowedToMove(side, counter)) {
+        const width = getVideoWidth();
         if (side === "left") {
           counter = counter - 1;
         }
 
-        const translate = counter * 265 - (side === "left" ? 265 : 0);
-        videosListWrp.style.transform = `translateX(-${translate}px)`;
+        translateContainer(width, side);
 
         if (side === "right") {
           counter = counter + 1;
@@ -37,12 +70,6 @@ import "./example.scss";
 
         filler.style.width = `${counter * 33.33333333}%`;
       }
-    }
-
-    function isAlowedToMove(side, counter) {
-      return (
-        (side === "left" && counter > 1) || (side === "right" && counter < 3)
-      );
     }
   });
 })();
