4186de7b08848900115df9a47f602bebcb37ea45
reyestimesbts <juanr@bluetrailsoft.com>
2021-05-14 12:15:03 -0500

    add menu button feature

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,19 @@
 export default function initOpenMenu() {
-  const btn = document.querySelector(".open");
+  const btn = document.querySelector(".close");
   const menu = document.querySelector(".menu");
   addClickEvnt(btn, menu);
 }
 
 function addClickEvnt(btn, menu) {
   btn.addEventListener("click", function () {
+    if (btn.classList.contains("close")) {
+      btn.classList.remove("close");
+      btn.classList.add("open");
+    } else {
+      btn.classList.add("close");
+      btn.classList.remove("open");
+    }
+
     if (menu.classList.contains("hide")) {
       menu.classList.remove("hide");
     } else {
