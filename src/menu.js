c977a15bb5d45c596378f63423e56fe69439d558
reyestimesbts <juanr@bluetrailsoft.com>
2021-05-17 08:59:32 -0500

    update calendar behavior

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,23 @@
 export default function initOpenMenu() {
-  const btn = document.querySelector(".close");
+  const btn = document.querySelector(".open");
   const menu = document.querySelector(".menu");
   addClickEvnt(btn, menu);
 }
 
 function addClickEvnt(btn, menu) {
   btn.addEventListener("click", function () {
-    if (btn.classList.contains("close")) {
-      btn.classList.remove("close");
-      btn.classList.add("open");
-    } else {
-      btn.classList.add("close");
+    if (btn.classList.contains("open")) {
       btn.classList.remove("open");
+      btn.classList.add("close");
+    } else {
+      btn.classList.add("open");
+      btn.classList.remove("close");
     }
 
-    if (menu.classList.contains("hide")) {
-      menu.classList.remove("hide");
+    if (menu.classList.contains("show")) {
+      menu.classList.remove("show");
     } else {
-      menu.classList.add("hide");
+      menu.classList.add("show");
     }
   });
 }
--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,9 @@
 import initVideo from "./video";
+import initOpenMenu from "./menu";
 
 (function () {
   document.addEventListener("DOMContentLoaded", function () {
     initVideo();
+    initOpenMenu();
   });
 })();
